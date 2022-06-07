2b891c04fa65dd99f9fe40022c66750b63beec9a
authura <rhoferdevs@gmail.com>
2022-06-07 11:04:15 -0400

    added fstring to print celsius

--- fahrenheit_to_celsius.py
+++ fahrenheit_to_celsius.py
@@ -1,6 +1,6 @@
-fahrenheit = input("Enter degrees in fahrenheit: ")
+fahrenheit = input("Enter degrees in Fahrenheit: ")
 
 #Python follows order of operations so long as parentheses are used
 celsius = (float(fahrenheit) - 32) * (5/9)
 
-print(celsius)
+print(f"Degrees in Celsius: {celsius}")
