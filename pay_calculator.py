c5769e0c64c43b63905bc39d99dfec846d11f7f5
authura <rhoferdevs@gmail.com>
2022-06-07 12:58:49 -0400

    moved pay calculation into function compute_pay

--- pay_calculator.py
+++ pay_calculator.py
@@ -1,15 +1,20 @@
+def compute_pay(hours, rate):
+    #Overtime is extra hours worked * 1.5
+    if hours > 40:
+        pay = ((hours - 40) * rate * 1.5) + rate * 40
+
+    else:
+        pay = hours * rate
+
+    return pay
 
 try:
     hours = float(input("Enter hours worked: "))
 
     rate = float(input("Enter rate: "))
 
-#Overtime is extra hours worked * 1.5
-    if hours > 40:
-        pay = ((hours - 40) * rate * 1.5) + rate * 40
+    pay = compute_pay(hours, rate)
 
-    else:
-        pay = hours * rate
 
     print(f"Gross pay: {pay}\n")
 
