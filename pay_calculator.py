0aa527b20f056af5576b35bc5d19f4a818826362
authura <rhoferdevs@gmail.com>
2022-06-07 11:49:32 -0400

    added try/except for non-numerical input

--- pay_calculator.py
+++ pay_calculator.py
@@ -1,12 +1,18 @@
-hours = float(input("Enter hours worked: "))
 
-rate = float(input("Enter rate: "))
+try:
+    hours = float(input("Enter hours worked: "))
+
+    rate = float(input("Enter rate: "))
 
 #Overtime is extra hours worked * 1.5
-if hours > 40:
-    pay = ((hours - 40) * rate * 1.5) + rate * 40
+    if hours > 40:
+        pay = ((hours - 40) * rate * 1.5) + rate * 40
 
-else:
-    pay = hours * rate
+    else:
+        pay = hours * rate
 
-print(f"Gross pay: {pay}\n")
+    print(f"Gross pay: {pay}\n")
+
+except:
+
+    print("Invalid input. Please enter a number.")
