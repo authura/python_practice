a3968ee8186039d485395554d6e2e957a0eea8b3
authura <rhoferdevs@gmail.com>
2022-06-07 12:28:40 -0400

    chain conditionals to determine if input is valid. If so, determine grade

---
+++ score_to_grade.py
@@ -0,0 +1,23 @@
+
+
+try:
+    score = float(input("Enter score between 0.0 and 1.0:\n"))
+
+    if score > 1 or score < 0:
+        print("Input out of range.")
+        quit()
+    elif score >= 0.9:
+        grade = 'A'
+    elif score >= 0.9:
+        grade = 'B'
+    elif score >= 0.7:
+        grade = 'C'
+    elif score >= 0.6:
+        grade = 'D'
+    else:
+        grade = 'F'
+
+
+    print(f"Grade: {grade}")
+except:
+    print("Invalid input.")
