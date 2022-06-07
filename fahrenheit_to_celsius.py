07e56f7b13add5b26e569f3f65b2ae81bc8bd64e
authura <rhoferdevs@gmail.com>
2022-06-07 11:01:25 -0400

    basic celsius conversion formula

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,6 @@
+fahrenheit = input("Enter degrees in fahrenheit: ")
+
+#Python follows order of operations so long as parentheses are used
+celsius = (float(fahrenheit) - 32) * (5/9)
+
+print(celsius)
