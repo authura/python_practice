ea876739096e06b4a9db7122a3837b1dbefb3779
authura <rhoferdevs@gmail.com>
2022-06-08 11:41:35 -0400

    update total and count values

--- number_reader.py
+++ number_reader.py
@@ -5,7 +5,11 @@ while True:
     n_string = input("Enter a number: ")
 
     try:
-        float(n_string)
+        n = float(n_string)
     except:
         print("Invalid input. Please enter a number.")
+        continue
+    
+    total = total + n
+    count = count + 1
 
