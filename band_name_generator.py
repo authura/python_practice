e064de18e12472255acc50128d07c9141cba8713
authura <rhoferdevs@gmail.com>
2022-06-17 11:25:56 -0400

    created simple word generator based on inputs

---
+++ band_name_generator.py
@@ -0,0 +1,11 @@
+name = input("Hey there! What's your name?\n\n")
+
+print(f"Nice to meet you, {name}!\n\n")
+
+print("Let's find out your band name!\n\n")
+
+city = input("What city did you grow up in?\n\n")
+
+pet = input("What's the name of your favorite pet?\n\n")
+
+print(f"Your band name is {city} {pet}!\n\n")
